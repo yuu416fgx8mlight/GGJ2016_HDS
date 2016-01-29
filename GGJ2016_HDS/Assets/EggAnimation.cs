6feacef95e5bdfb5f5c3da4f07ee0dce49b2294d
nepia12 <0fm38r05348532h@ezweb.ne.jp>
2016-01-30 03:34:03 +0900

    卵揺れ

---
+++ GGJ2016_HDS/Assets/EggAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EggAnimation : MonoBehaviour {
+	Animator anime;
+	// Use this for initialization
+	void Start () {
+		anime = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		anime.SetTrigger ("Shake");
+		Debug.Log ("hit");
+	}
+}
