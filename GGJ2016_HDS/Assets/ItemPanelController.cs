cf4dd21ff5f23fb11e3f07f915faec22e6140cd3
Kamiura Takuto <taku.second@gmail.com>
2016-01-30 04:36:22 +0900

    アイテムUIのポップアップ

---
+++ GGJ2016_HDS/Assets/ItemPanelController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPanelController : MonoBehaviour {
+	public GameObject syncPanel;
+	bool up=false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick() { 
+		if(up==false){
+			syncPanel.GetComponent<Animator> ().SetBool ("Up",true);
+			up = true;
+		}else{syncPanel.GetComponent<Animator> ().SetBool ("Up", false);up = false;}
+
+	}
+}
