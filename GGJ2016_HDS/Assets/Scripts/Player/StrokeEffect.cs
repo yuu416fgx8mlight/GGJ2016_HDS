6e8138a63fb973c073f6fd39e7c40d3bfe2215d9
yuta <yamaguchiyuuta@yamaguchiyuuta.local>
2016-01-30 10:00:54 +0900

    不要アセットの削除

---
+++ GGJ2016_HDS/Assets/Scripts/Player/StrokeEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrokeEffect : MonoBehaviour {
+
+	private Vector3 MousePosition;
+	private Vector3 ScreenToWorldPointPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//押している間エフェクトがついてくる
+		if (Input.GetMouseButton (0)) {
+			MousePosition = Input.mousePosition;
+			MousePosition.z = 10f;
+			ScreenToWorldPointPosition = Camera.main.ScreenToWorldPoint (MousePosition);
+			this.transform.position = ScreenToWorldPointPosition;
+		} else {
+			//なんか止められないのでゴリ押し
+			this.transform.position = new Vector3(1000,0,0);
+		}
+	}
+}
---
+++ GGJ2016_HDS/Assets/Scripts/Player/Touch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour {
+
+	public GameObject TouchEffect;
+
+	private Vector3 TouchPosition;
+	private Vector3 TouchWorldPointPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			TouchPosition = Input.mousePosition;
+			TouchPosition.z = 10f;
+			TouchWorldPointPosition = Camera.main.ScreenToWorldPoint (TouchPosition);
+			Instantiate (TouchEffect, TouchWorldPointPosition, Quaternion.identity);
+		}
+	}
+}
