dc731c8eeb1f372b8718a4fe4cbd446149b2f967
高橋直希 <kashisu.enoki@gmail.com>
2016-01-30 16:31:59 +0900

    バグ修正

--- GGJ2016_HDS/Assets/Takahashi/Script/Equipment/EquipmentNode.cs
+++ GGJ2016_HDS/Assets/Takahashi/Script/Equipment/EquipmentNode.cs
@@ -5,6 +5,7 @@ public class EquipmentNode : MonoBehaviour {
     //とりあえずつかうときに使う
     public MasterShop.param info;
     public EquipmentType type;
+    public Transform equipparent;
     public void NodeClick(EquipmentGridType type,EquipmentGrid grid,MasterShop.param data)
     {
         if (type == EquipmentGridType.Category)
@@ -13,8 +14,17 @@ public class EquipmentNode : MonoBehaviour {
         }
         if (type == EquipmentGridType.Item)
         {
+            int category = data.category;
             DialogCreater.CreateShopDialog(CanvasList.Get.GetCanvas(CanvasType.FrontCanvas).point.transform,
-                    data);
+                    data,()=> { SetEquipment((EquipmentType)0); });
+        }
+    }
+    public void SetEquipment(EquipmentType type)
+    {
+        if (EquipmentType.Hand == type)
+        {
+            GameObject g=Instantiate(ResourceManager.Get.GetPrefab("Image"))as GameObject;
+            g.transform.SetParent(EquipmentPointSystem.Get.HandPoint,false);
         }
     }
 
---
+++ GGJ2016_HDS/Assets/Takahashi/Script/Equipment/EquipmentPointSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EquipmentPointSystem : MonoBehaviour {
+    public static EquipmentPointSystem Get;
+    public Transform HandPoint;
+    public Transform LagPiont;
+    public Transform LightPoint;
+    void Awake()
+    {
+        Get = this;
+        foreach(Transform i in transform)
+        {
+            if (i.name == "HandPoint") HandPoint = i;
+            if (i.name == "LagPoint") LagPiont = i;
+            if (i.name == "LightPoint")LightPoint = i;
+        }
+
+    }
+
+}
