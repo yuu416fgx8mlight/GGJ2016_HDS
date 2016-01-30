5b0370a9a2d5283df0dbfd75a7814daad1c8b729
yuta <yamaguchiyuuta@yamaguchiyuuta.local>
2016-01-30 17:23:43 +0900

    タイトルシーン遷移の追加

---
+++ GGJ2016_HDS/Assets/Scripts/TitleButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleButton : MonoBehaviour {
+
+	//2重呼び出し防止用フラグ
+	private bool SceneFrag;
+
+	// Use this for initialization
+	void Start () {
+		SceneFrag = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnStartClick() {
+		if (!SceneFrag) {
+			SceneFrag = true;
+			FadeManager.Instance.LoadLevel ("Main", 1.0f);
+		}
+	}
+
+	public void OnCreditClick(){
+		if (!SceneFrag) {
+			SceneFrag = true;
+			FadeManager.Instance.LoadLevel ("Credit", 1.0f);
+		}
+	}
+}
