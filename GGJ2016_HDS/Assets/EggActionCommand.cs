baa2a0f9ca2ef7b7186272a71adb71706647b39e
nepia12 <0fm38r05348532h@ezweb.ne.jp>
2016-01-30 21:34:56 +0900

    画面サイズ

---
+++ GGJ2016_HDS/Assets/EggActionCommand.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EggActionCommand : MonoBehaviour {
+	private GameObject Butten;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OnClick()
+	{Debug.Log ("click");
+	}
+}
---
+++ GGJ2016_HDS/Assets/ScreenSizeChange.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSizeChange : MonoBehaviour {
+	public int ScreenWidth;
+	public int ScreenHeight;
+	// Use this for initialization
+
+	void Awake(){
+		if (Application.platform == RuntimePlatform.WindowsPlayer ||
+		   Application.platform == RuntimePlatform.OSXPlayer ||
+		   Application.platform == RuntimePlatform.LinuxPlayer) {
+			Screen.SetResolution (ScreenWidth, ScreenHeight, false);
+		}
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
